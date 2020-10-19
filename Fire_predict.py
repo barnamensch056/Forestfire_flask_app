a10b1652ae5f3476e3b382a6906eea541623fa2f
barnamensch056 <barnadipdey@gmail.com>
2020-10-19 12:43:48 +0530

    templates are modified

--- Fire_predict.py
+++ Fire_predict.py
@@ -10,18 +10,28 @@ def home():
 
 @app.route('/predict',methods=['GET','POST'])
 def predict():
+    ''' A function to measure the 
+    predictibility of forest fiore'''
     characters1=[int(y) for y in request.form.values()]
     charac2=[np.array(characters1)]
-    print(characters1)
-    print(charac2)
-    prediction=fire_pickle.predict_proba(charac2)
-    print(prediction)
-    output='{0:.{1}f}'.format(prediction[0][1], 1)
-    print(output,type(output))
-    if output<='0.5':
-        return render_template('onfire.html',pred_text='Fire will not occur,forest is marked safe ')
-    elif output>='0.5':
-        return render_template('onfire.html',pred_text='The forest will be on fire,Run!!')
+    #print(characters1)
+    #print(charac2)
+    probability=fire_pickle.predict_proba(charac2)
+
+    prediction=fire_pickle.predict(charac2)
+
+    probability.astype(int)
+    prediction.astype(int)
+    print(prediction,type(prediction))
+    probabs=round(probability[0][1], 2)
+    predicts=prediction[0]
+    print(predicts)
+    #print(probabs,type(probabs))
+    #print(predicts,type(predicts))
+    if probabs<=0.5 and predicts==0:
+        return render_template('onfire.html',pred_text='Fire will not occur,forest is marked safe and probability of occuring fire is {}'.format(probabs))
+    elif probabs>=0.5 and predicts==1:
+        return render_template('onfire.html',pred_text='The forest will be on fire Run!! and probability of occuring fire is {}'.format(probabs))
     else:
         return render_template('onfire.html',pred_text='Jangal pe mangal not allowed')
 
